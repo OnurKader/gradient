eb1fb7aea29ad101a397ec207eb1ab8137a1f6c2
OnurKader <onurorkunkader1999@gmail.com>
2019-11-29 21:26:37 +0000

    Added animation, doesn't delete or go to 0,0 successfuly

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "color.c"
 
 #include <getopt.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
@@ -16,8 +17,8 @@ void getTermSize(short* width, short* height)
 
 void gradient(const short width, const short height, color_t* first, color_t* second)
 {
-	rgb2hsv(first);
-	rgb2hsv(second);
+	/* rgb2hsv(first); */
+	/* rgb2hsv(second); */
 	for(short row = 0; row < height; ++row)
 	{
 		for(short col = 0; col < width; ++col)
@@ -29,14 +30,22 @@ void gradient(const short width, const short height, color_t* first, color_t* se
 			printf("%s ", color.str);
 		}
 		printf("\n");
-		first->h = fmod((first->h + 1.f), 360.f);
-		second->h = fmod((second->h + 1.f), 360.f);
+		first->h = fmod((first->h + 0.018f), 360.f);
+		second->h = fmod((second->h + 0.018f), 360.f);
 	}
-	printf("%s", RESET);
+}
+
+void signal_handle(int sig)
+{
+	printf("%d\b\033[?25h", sig);
+	exit(0);
 }
 
 int main(int argc, const char** argv)
 {
+	signal(SIGINT, signal_handle);
+	signal(SIGTERM, signal_handle);
+
 	color_t initial_color;
 	color_t ending_color;
 	initial_color.r = 229;
@@ -46,8 +55,18 @@ int main(int argc, const char** argv)
 	ending_color.g = 206;
 	ending_color.b = 241;
 
+	rgb2hsv(&initial_color);
+	rgb2hsv(&ending_color);
+	printf("\033[?25l");
 	short width, height;
-	getTermSize(&width, &height);
-	gradient(width, height, &initial_color, &ending_color);
+	printf("\033[2J");
+	while(1)
+	{
+		getTermSize(&width, &height);
+		printf("\033[H");
+		gradient(width, height, &initial_color, &ending_color);
+		usleep(40000U);
+	}
+	printf("%s", RESET);
 	return 0;
 }
